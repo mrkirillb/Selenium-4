e171f48fba7125d762b3221ef898d78b7cbc2b2e
mrkirillb <mrkirillb@gmail.com>
2021-12-06 22:11:50 +0300

    Выполнил задание №2

--- src/test/java/InsuranceTest.java
+++ src/test/java/InsuranceTest.java
@@ -1,95 +1,69 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.ClickButtonPage;
+import pages.MainPage;
+import pages.OptionsInsurancePage;
+import pages.SendAppPage;
 
-import java.time.Duration;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.assertEquals;
-
-public class InsuranceTest {
-    private WebDriver driver;
-    private String baseUrl;
-
-    @Before
-    public void testInsurence () {
-        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
-        baseUrl = "https://www.sberbank.ru/ru/person";
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.manage().window().maximize();
-        driver.get(baseUrl);
-    }
+public class InsuranceTest extends BaseTest {
 
     @Test
-    public void InsuranceTest () throws InterruptedException {
-        driver.findElement(By.xpath ("//ul[contains(@class,'kitt-top-menu__list_center')]/li/*[contains(text(),'Страхование')]")).click();
-        driver.findElement(By.xpath ("//*[contains(text(),'Все страховые программы')]")).click();
-        driver.findElement(By.xpath ("//div[contains(@class,'product-catalog__carousel_row')]/div[contains(@data-product,'Страхование путешественников')]")).click();
-        Wait<WebDriver> wait = new WebDriverWait(driver, 10, 3000);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[contains(text(),'Страхование путешественников')][contains(@class,'kitt-heading')]"))));
-        assertEquals("Страхование путешественников",driver.findElement(By.xpath("//*[contains(text(),'Страхование путешественников')][contains(@class,'kitt-heading')]")).getText());
-        if(!driver.findElements(By.xpath("//div[contains(@class,'kitt-cookie-warning')][contains(@style,'display: block;')]//button[contains(@class,'kitt-cookie-warning__close')]")).isEmpty()){
-            driver.findElement(By.xpath("//button[contains(@class,'kitt-cookie-warning__close')]")).click();
-        }
-        driver.findElement(By.xpath("//span[contains(text(),'Оформить на сайте')]")).click();
-        for(String winHandle : driver.getWindowHandles()) {
+    public void InsuranseTest () {
+        driver.get(baseUrl);
+        MainPage mainPage = new MainPage(driver);
+        mainPage.waitSendAppClickable();
+        mainPage.selectMainMenu("Страхование");
+        mainPage.selectSubMenu("Путешествия");
+
+        ClickButtonPage clickOnlineButtonPage = new ClickButtonPage(driver);
+
+        String actualTitle = clickOnlineButtonPage.title.getText();
+        String expectedTitle = "Страхование путешественников";
+        assertTrue(String.format("Заголовок равен [%s]. Ожидалось - [%s]", actualTitle, expectedTitle), actualTitle.contains(expectedTitle));
+        clickOnlineButtonPage.sendButton.click();
+        clickOnlineButtonPage.sendButton2.click();
+
+        for(String winHandle : driver.getWindowHandles()){
             driver.switchTo().window(winHandle);
         }
-        driver.findElement(By.xpath("//h3[contains(text(),'Минимальная')]")).click();
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//button[contains(text(),'Оформить')]")))).click();
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//legend[contains(text(),'Застрахованные')]"))));
-        fillField(By.xpath("//input[contains(@placeholder, 'Surname')]"), "Буаев");
-        fillField(By.xpath("//input[contains(@placeholder, 'Name')]"), "Крил");
-        fillField(By.xpath("//input[contains(@placeholder, 'дд.мм.гггг')]"), "10.01.1963");
 
-        fillField(By.id("person_firstName"), "Крил");
-        fillField(By.id("person_lastName"), "Буаев");
-        fillField(By.id("person_middleName"), "Втальевч");
-        fillField(By.id("person_birthDate"), "10.01.1963");
-        driver.findElement(By.xpath("//input[contains(@id,'person_firstName')]")).click();
+        OptionsInsurancePage optionInsurancePage = new OptionsInsurancePage(driver);
+        optionInsurancePage.selectType("Минимальная");
+        optionInsurancePage.sendButton("Оформить");
 
-        driver.findElement(By.xpath("//label[contains(@class,'btn ng-untouched ng-pristine ng-valid')][contains(text(),'Мужской')]")).click();
+        SendAppPage sendAppPage = new SendAppPage(driver);
+        sendAppPage.fillField("Фамилия застрахованного", "Будаев");
+        sendAppPage.fillField("Имя застрахованного", "Кирилл");
+        sendAppPage.fillField("Дата рождения застрахованного", "10.03.1994");
+        sendAppPage.fillField("Фамилия страхователя", "Будаев");
+        sendAppPage.fillField("Имя страхователя", "Кирилл");
+        sendAppPage.fillField("Отчество страхователя", "Витальевич");
+        sendAppPage.fillField("Дата рождения страхователя", "10.03.1994");
+        sendAppPage.fillField("Серия паспорта", "0000");
+        sendAppPage.fillField("Номер паспорта", "777777");
+        sendAppPage.fillField("Дата выдачи", "16.10.2020");
+        sendAppPage.fillField("Кем выдан", "Отделом ФМС");
+        sendAppPage.male.click();
 
-        fillField(By.id("passportSeries"), "1234");
-        fillField(By.id("passportNumber"), "567890");
-        fillField(By.id("documentDate"), "10.01.2020");
-        fillField(By.id("documentIssue"), "ФГБОУ");
 
-        assertEquals("Буаев", driver.findElement(By.xpath("//input[contains(@placeholder,'Surname')]")).getAttribute("value"));
-        assertEquals("Крил", driver.findElement(By.xpath("//input[contains(@placeholder,'Name')]")).getAttribute("value"));
-        assertEquals("10.01.1963", driver.findElement(By.xpath("//input[contains(@placeholder,'дд.мм.гггг')]")).getAttribute("value"));
-        assertEquals("Буаев", driver.findElement(By.id("person_lastName")).getAttribute("value"));
-        assertEquals("Крил", driver.findElement(By.id("person_firstName")).getAttribute("value"));
-        assertEquals("Втальевч", driver.findElement(By.id("person_middleName")).getAttribute("value"));
-        assertEquals("10.01.1963", driver.findElement(By.id("person_birthDate")).getAttribute("value"));
-        assertEquals("1234", driver.findElement(By.id("passportSeries")).getAttribute("value"));
-        assertEquals("567890", driver.findElement(By.id("passportNumber")).getAttribute("value"));
-        assertEquals("10.01.2020", driver.findElement(By.id("documentDate")).getAttribute("value"));
-        assertEquals("ФГБОУ", driver.findElement(By.id("documentIssue")).getAttribute("value"));
+        sendAppPage.getFillField("Фамилия застрахованного", "Будаев");
+        sendAppPage.getFillField("Имя застрахованного", "Кирилл");
+        sendAppPage.getFillField("Дата рождения застрахованного", "10.03.1994");
+        sendAppPage.getFillField("Фамилия страхователя", "Будаев");
+        sendAppPage.getFillField("Имя страхователя", "Кирилл");
+        sendAppPage.getFillField("Отчество страхователя", "Витальевич");
+        sendAppPage.getFillField("Дата рождения страхователя", "10.03.1994");
+        sendAppPage.getFillField("Серия паспорта", "0000");
+        sendAppPage.getFillField("Номер паспорта", "777777");
+        sendAppPage.getFillField("Дата выдачи", "16.10.2020");
+        sendAppPage.getFillField("Кем выдан", "Отделом ФМС");
 
-        driver.findElement(By.xpath("//button[contains(text(),'Продолжить')]")).click();
+        sendAppPage.sendButton.click();
 
-        assertEquals("При заполнении данных произошла ошибка", driver.findElement(By.xpath("//div[contains(@class,'alert-form alert-form-error')]")).getAttribute("innerText"));
-        assertEquals("Поле не заполнено.", driver.findElement(By.xpath("//form-control-label[contains(@title,'Мобильный телефон')]//span[contains(@class,'message')]")).getAttribute("innerText"));
-        assertEquals("Поле не заполнено.", driver.findElement(By.xpath("//form-control-label[contains(@title,'Электронная почта')]//span[contains(@class,'message')]")).getAttribute("innerText"));
-        assertEquals("Поле не заполнено.", driver.findElement(By.xpath("//form-control-label[contains(@title,'Повтор электронной почты')]//span[contains(@class,'message')]")).getAttribute("innerText"));
-
+        sendAppPage.checkFieldErrorMessage("Мобильный телефон", "Поле не заполнено.");
+        sendAppPage.checkFieldErrorMessage("Электронная почта", "Поле не заполнено.");
+        sendAppPage.checkFieldErrorMessage("Повтор электронной почты", "Поле не заполнено.");
     }
 
-    @After
-    public void afterTest(){
-        driver.quit();
-    }
-
-    private void fillField(By locator, String value) throws InterruptedException {
-        driver.findElement(locator).click();
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(value);
-    }
 }
